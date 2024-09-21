7e826c7ef72a11e85f0378926fea110a20eb64a9
Sachin Bamniya <159703523+SACHIN-BAMNIYA@users.noreply.github.com>
2024-09-22 02:04:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
-// const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".input");
 
-// function focusFunc() {
-//   let parent = this.parentNode;
-//   parent.classList.add("focus");
-// }
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
 
-// function blurFunc() {
-//   let parent = this.parentNode;
-//   if (this.value == "") {
-//     parent.classList.remove("focus");
-//   }
-// }
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
 
-// inputs.forEach((input) => {
-//   input.addEventListener("focus", focusFunc);
-//   input.addEventListener("blur", blurFunc);
-// });
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const projectCards = document.querySelectorAll('.project-card');
+document.addEventListener('DOMContentLoaded', () => {
+  const projectCards = document.querySelectorAll('.project-card');
+  projectCards.forEach(card => {
+    card.addEventListener('click', () => {
+      // Your code for handling card click can go here
+    });
+  });
+});
 
-//   projectCards.forEach(card => {
-//       card.addEventListener('click', () => {
+/* PROJECT */
+function openModal(card) {
+  const modal = document.querySelector('#modal');
+  const modalContent = modal.querySelector('.modal-content');
+  const projectTitle = card.querySelector('h3').innerText;
+  const projectDescription = card.querySelector('p').innerText;
 
-//       });
-//   });
-// });
+  modalContent.querySelector('h3').innerText = projectTitle;
+  modalContent.querySelector('p').innerText = projectDescription;
 
-/*PROJECT*/
+  modal.style.display = 'block';
+}
 
-// function openModal(card) {
-//   const modal = document.querySelector('#modal');
-//   const modalContent = modal.querySelector('.modal-content');
-//   const projectTitle = card.querySelector('h3').innerText;
-//   const projectDescription = card.querySelector('p').innerText;
+function closeModal() {
+  const modal = document.querySelector('#modal');
+  modal.style.display = 'none';
+}
 
-//   modalContent.querySelector('h3').innerText = projectTitle;
-//   modalContent.querySelector('p').innerText = projectDescription;
-
-//   modal.style.display = 'block';
-// }
-
-// function closeModal() {
-//   const modal = document.querySelector('#modal');
-//   modal.style.display = 'none';
-// }
-
-/*SIDEBAR*/ 
+/* SIDEBAR */
+function openNav() {
+    document.getElementById("mySidenav").style.left = "0";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.left = "-250px";
+  }
